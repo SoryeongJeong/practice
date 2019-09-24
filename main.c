a545b67777b12ac43466a97b03248cc6c61c756b
SoryeongJeong <55128033+SoryeongJeong@users.noreply.github.com>
2019-09-24 16:41:15 +0900

    Initial commit

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void)
+
+{ 	int x;
+
+	printf("bytes of int : %d\n", sizeof(x));
+
+	
+
+	char y;
+
+	printf("bytes of char : %d\n", sizeof(y));
+
+	
+
+	short z;
+
+	printf("bytes of short : %d\n", sizeof(z));
+
+	
+
+	float k;
+
+	printf("bytes of float : %d\n", sizeof(k));
+
+	
+
+	double w;
+
+	printf("bytes of double : %d\n", sizeof(w));
+
+	
+
+	return 0;
+
+}
